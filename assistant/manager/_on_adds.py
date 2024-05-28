cc6cc9cd5b61202def33598da3442edb578983c8
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-05-28 16:42:17 +0530

    Update _on_adds.py

--- assistant/manager/_on_adds.py
+++ assistant/manager/_on_adds.py
@@ -1,50 +1,23 @@
-# Moonshining1 - UserBot
-# Copyright (C) 2021-2023 User-bot2.0
+# Ultroid - UserBot
+# Copyright (C) 2021-2023 TeamUltroid
 #
-# This file is a part of < https://github.com/Moonshining1/User-bot2.0/ >
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/Moonshining1/User-bot2.0/blob/main/LICENSE/>.
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
-import re
-
-from telethon.errors.rpcerrorlist import UserNotParticipantError
-
-from pyUltroid import _ult_cache
+from telethon import events
 
 from . import *
 
 
-@ultroid_cmd(pattern="d(kick|ban)", manager=True, require="ban_users")
-async def dowj(e):
-    replied = await e.get_reply_message()
-    if replied:
-        user = replied.sender_id
-    else:
-        return await e.eor("Reply to a message...")
-    try:
-        await replied.delete()
-        if e.pattern_match.group(1).strip() == "kick":
-            await e.client.kick_participant(e.chat_id, user)
-            te = "Kicked"
-        else:
-            await e.client.edit_permissions(e.chat_id, user, view_messages=False)
-            te = "Banned"
-        await e.eor(f"{te} Successfully!")
-    except Exception as E:
-        await e.eor(str(E))
-
-
-@callback(re.compile("cc_(.*)"), func=_ult_cache.get("admin_callback"))
-async def callback_(event):
-    data = event.data_match.group(1).decode("utf-8")
-    if data not in _ult_cache.get("admin_callback", {}):
+@asst.on(events.ChatAction(func=lambda x: x.user_added))
+async def dueha(e):
+    user = await e.get_user()
+    if not user.is_self:
         return
-    try:
-        perm = await event.client.get_permissions(event.chat_id, event.sender_id)
-    except UserNotParticipantError:
-        return await event.answer("Join the Group First!", alert=True)
-    if not perm.is_admin:
-        return await event.answer("You are not an Admin!", alert=True)
-    _ult_cache["admin_callback"].update({data: (event.sender, perm)})
-    await event.answer("Verification Done!")
-    await event.delete()
+    sm = udB.get_key("ON_MNGR_ADD")
+    if sm == "OFF":
+        return
+    if not sm:
+        sm = "Thanks for Adding me :)"
+    await e.reply(sm, link_preview=False)
